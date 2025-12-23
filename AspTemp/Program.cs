93655db8a938c0b78d4aeb891dec2d66550c366b
Haftom <haftish.tsegay@gmail.com>
2025-12-23 17:12:51 +0300

    feat: configurations separated

--- AspTemp/Program.cs
+++ AspTemp/Program.cs
@@ -1,8 +1,17 @@
+using AspTemp.Features.Auth.Commands;
+using AspTemp.Shared.Application;
+using AspTemp.Shared.Application.Contracts.ResultContracts.Extensions;
+using MediatR;
+
+var assembly = typeof(Program).Assembly;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.ConfigureApplication(assembly);
 
 var app = builder.Build();
 
@@ -14,28 +23,18 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
+app.MapPost("/auth/signin", async (SignIn request, ISender sender, CancellationToken ct) =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
+        var result = await sender.Send(request, ct);
+        return result.ToHttpResult();
     })
     .WithName("GetWeatherForecast");
 
+app.MapGet("/paginated", async ([AsParameters] GetAllUsers request, ISender sender, CancellationToken ct) =>
+{
+    var result = await sender.Send(request, ct);
+    return result.ToHttpResult();
+});
+
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ AspTemp/Shared/Application/Configurations.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using AspTemp.Shared.Application.Contracts.Cqrs;
+using FluentValidation;
+
+namespace AspTemp.Shared.Application;
+
+public static class Configurations
+{
+    public static void ConfigureApplication(this IServiceCollection services, Assembly assembly)
+    {
+        services.AddValidatorsFromAssembly(assembly, lifetime: ServiceLifetime.Scoped);
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(assembly);
+            config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+            // config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+    }
+}
